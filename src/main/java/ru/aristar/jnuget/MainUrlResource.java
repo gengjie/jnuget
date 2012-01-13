c28f5629a9f3a5066858865449a0a806cd64fb0a
Sviridov Dmitry <sviridov@neolant.ru>
2012-01-13 13:57:41 +0400

    * ????????? ?????? ??? ??????? ? ????? * ???????? ????? EntryProperties

---
+++ src/main/java/ru/aristar/jnuget/MainUrlResource.java
@@ -0,0 +1,172 @@
+package ru.aristar.jnuget;
+
+import ru.aristar.jnuget.rss.MainUrl;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.aristar.jnuget.files.NupkgFile;
+import ru.aristar.jnuget.rss.PackageEntry;
+import ru.aristar.jnuget.rss.PackageEntryNameComparator;
+import ru.aristar.jnuget.rss.PackageFeed;
+import ru.aristar.jnuget.sources.FilePackageSource;
+
+/**
+ * REST Web Service
+ *
+ * @author sviridov
+ */
+@Path("")
+public class MainUrlResource {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of MainUrlResource
+     */
+    public MainUrlResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of
+     * ru.aristar.jnuget.MainUrlResource
+     *
+     * @return
+     */
+    @GET
+    @Produces("application/xml")
+    @Path("nuget")
+    public Response getXml() {
+        StringWriter writer = new StringWriter();
+        try {
+            MainUrl mainUrl = new MainUrl(context.getAbsolutePath().toString());
+            mainUrl.writeXml(writer);
+        } catch (JAXBException e) {
+            final String errorMessage = "Ошибка преобразования XML";
+            logger.error(errorMessage, e);
+            return Response.serverError().entity(errorMessage).build();
+        }
+        return Response.ok(writer.toString(), MediaType.APPLICATION_XML).build();
+    }
+
+    @GET
+    @Produces("application/xml")
+    @Path("nuget/{metadata : [$]metadata}")
+    public Response getMetadata() {
+        InputStream inputStream = this.getClass().getResourceAsStream("/metadata.xml");
+        ResponseBuilder response = Response.ok((Object) inputStream);
+        return response.build();
+    }
+
+    @GET
+    @Produces("application/xml")
+    @Path("nuget/Packages")
+    public Response getPackages() {
+        try {
+            //Фейковая реализация
+            File file = new File("c:/inetpub/wwwroot/nuget/Packages/");
+            FilePackageSource packageSource = new FilePackageSource(file);
+            PackageFeed feed = new PackageFeed();
+            feed.setId(context.getAbsolutePath().toString());
+            feed.setUpdated(new Date());
+            feed.setTitle("Packages");
+            ArrayList<PackageEntry> packageEntrys = new ArrayList<>();
+            NugetContext nugetContext = new NugetContext(context.getBaseUri());
+            for (NupkgFile nupkg : packageSource.getPackages()) {
+                PackageEntry entry = nugetContext.createPackageEntry(nupkg);
+                packageEntrys.add(entry);
+            }
+            Collections.sort(packageEntrys, new PackageEntryNameComparator());
+            feed.setEntries(packageEntrys);
+            //конец реализации
+            return Response.ok(feed.getXml(), MediaType.APPLICATION_ATOM_XML_TYPE).build();
+        } catch (JAXBException e) {
+            final String errorMessage = "Ошибка преобразования XML";
+            logger.error(errorMessage, e);
+            return Response.serverError().entity(errorMessage).build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_ATOM_XML)
+    @Path("nuget/{search : Search[(][)]}")
+    //Search()?$filter=IsLatestVersion&$orderby=Id&$skip=0&$top=30&searchTerm=''&targetFramework=''
+    public Response search(@QueryParam("$filter") String filter,
+            @QueryParam("$orderby") String orderBy,
+            @QueryParam("$skip") String skip,
+            @QueryParam("searchTerm") String searchTerm,
+            @QueryParam("targetFramework") String targetFramework) {
+        try {
+            //Фейковая реализация
+            File file = new File("c:/inetpub/wwwroot/nuget/Packages/");
+            FilePackageSource packageSource = new FilePackageSource(file);
+            PackageFeed feed = new PackageFeed();
+            feed.setId(context.getAbsolutePath().toString());
+            feed.setUpdated(new Date());
+            feed.setTitle("Search");
+            ArrayList<PackageEntry> packageEntrys = new ArrayList<>();
+            NugetContext nugetContext = new NugetContext(context.getBaseUri());
+            for (NupkgFile nupkg : packageSource.getPackages()) {
+                PackageEntry entry = nugetContext.createPackageEntry(nupkg);
+                packageEntrys.add(entry);
+            }
+            Collections.sort(packageEntrys, new PackageEntryNameComparator());
+            feed.setEntries(packageEntrys);
+            //конец реализации
+            return Response.ok(feed.getXml(), MediaType.APPLICATION_ATOM_XML_TYPE).build();
+        } catch (JAXBException e) {
+            final String errorMessage = "Ошибка преобразования XML";
+            logger.error(errorMessage, e);
+            return Response.serverError().entity(errorMessage).build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    @Path("download/{id}/{version}")
+    public Response getPackage(@PathParam("id") String id,
+            @PathParam("version") String versionString) {
+        try {
+            Version version = Version.parse(versionString);
+            //Фейковая реализация
+            File file = new File("c:/inetpub/wwwroot/nuget/Packages/");
+            FilePackageSource packageSource = new FilePackageSource(file);
+            NupkgFile nupkg = packageSource.getPackage(id, version);
+            InputStream inputStream = nupkg.getStream();
+            ResponseBuilder response = Response.ok((Object) inputStream);
+            response.type(MediaType.APPLICATION_OCTET_STREAM);
+            String fileName = nupkg.getFileName();
+            response.header("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+            return response.build();
+        } catch (Exception e) {
+            final String errorMessage = "Ошибка преобразования XML";
+            logger.error(errorMessage, e);
+            return Response.serverError().entity(errorMessage).build();
+        }
+    }
+
+    /**
+     * PUT method for updating or creating an instance of MainUrlResource
+     *
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+}
---
+++ src/main/java/ru/aristar/jnuget/Version.java
@@ -0,0 +1,173 @@
+package ru.aristar.jnuget;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Версия пакета
+ * @author unlocker
+ */
+public class Version implements Comparable<Version> {
+
+    /**
+     * Выражение разбора
+     */
+    private static Pattern pattern = Pattern.compile("(\\d+)\\.?(\\d*)\\.?(\\d*)\\.?(.*)");
+
+    
+    private static Integer parseInt(String group) {
+        if (group == null || group.isEmpty()) {
+            return null;
+        } else {
+            return Integer.parseInt(group);
+        }
+    }
+    private final Integer major;
+    private final Integer minor;
+    private final Integer build;
+    private final String revision;
+
+    public Version(Integer major, Integer minor, Integer build, String revision) {
+        this.major = major;
+        this.minor = minor;
+        this.build = build;
+        this.revision = revision;
+    }
+
+    Version(Object[] object) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public Integer getMajor() {
+        return major;
+    }
+
+    public Integer getMinor() {
+        return minor;
+    }
+
+    public Integer getBuild() {
+        return build;
+    }
+
+    public String getRevision() {
+        return revision;
+    }
+
+    public static Version parse(String input) throws Exception {
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.find()) {
+            throw new Exception("Аргумент неправильный. " + input);
+        }
+        Integer major = parseInt(matcher.group(1));
+        Integer minor = parseInt(matcher.group(2));
+        Integer build = parseInt(matcher.group(3));
+        String revision = matcher.group(4);
+        return new Version(major, minor, build, revision);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Version)) {
+            return false;
+        } else {
+            Version o = (Version) obj;
+            return this.major == o.major
+                    && this.minor == o.minor
+                    && this.build == o.build
+                    && stringsNullOrEqual(this.revision, o.revision);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.major);
+        hash = 89 * hash + Objects.hashCode(this.minor);
+        hash = 89 * hash + Objects.hashCode(this.build);
+        hash = 89 * hash + Objects.hashCode(this.revision);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(major.intValue());
+        if (minor != null) {
+            buffer.append(".").append(minor.intValue());
+
+            if (build != null) {
+                buffer.append(".").append(build.intValue());
+
+                if (revision != null && !revision.trim().isEmpty()) {
+                    if (!revision.startsWith("-")) {
+                        buffer.append(".");
+                    }
+                    buffer.append(revision);
+                }
+            }
+        }
+
+
+        return buffer.toString();
+    }
+
+    public int compareTo(Version o) {
+        if (this.equals(o)) {
+            return 0;
+        } else {
+            if (this.major > o.major
+                    || (this.major == o.major && compareIntegerPossibleNull(this.minor, o.minor) > 0)
+                    || (this.major == o.major && this.minor == o.minor && compareIntegerPossibleNull(this.build, o.build) > 0)
+                    || (this.major == o.major && this.minor == o.minor && this.build == o.build
+                    && compareStringPossibleNull(this.revision, o.revision) > 0)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    private int compareStringPossibleNull(String str1, String str2) {
+        if (stringsNullOrEqual(str1, str2)) {
+            return 0;
+        } else {
+            if (str1 == null && str2 != null) {
+                return 1;
+            } else if (str1 != null && str2 == null) {
+                return -1;
+            } else {
+                return str1.compareToIgnoreCase(str2);
+            }
+        }
+    }
+
+    private int compareIntegerPossibleNull(Integer int1, Integer int2) {
+        if (int1 == null && int2 == null) {
+            return 0;
+        } else {
+            if (int1 == null && int2 != null) {
+                return -1;
+            } else if (int1 != null && int2 == null) {
+                return 1;
+            } else {
+                return int1.compareTo(int2);
+            }
+        }
+    }
+
+    private boolean stringsNullOrEqual(String str1, String str2) {
+        if (str1 == null && str2 == null) {
+            return true;
+        }
+        if (str1 == null || str2 == null) {
+            return false;
+        }
+        // Сравнение без учета регистра.
+        return str1.toLowerCase().equals(str2.toLowerCase());
+    }
+}
