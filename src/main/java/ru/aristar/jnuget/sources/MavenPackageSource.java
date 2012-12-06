5f26051fff6e5ec438ae14c7b69cc553e56f4df2
Serg Maksimov <maksimov.sg@gmail.com>
2012-12-06 18:11:22 +0400

    ?????? ?????????? ????????? Maven.

--- src/main/java/ru/aristar/jnuget/sources/MavenPackageSource.java
+++ src/main/java/ru/aristar/jnuget/sources/MavenPackageSource.java
@@ -1,13 +1,26 @@
 package ru.aristar.jnuget.sources;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.DefaultArtifactRepository;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.maven.index.Indexer;
+import org.apache.maven.index.context.IndexCreator;
 import ru.aristar.jnuget.Version;
 import ru.aristar.jnuget.files.MavenNupkg;
 import ru.aristar.jnuget.files.Nupkg;
 import ru.aristar.jnuget.sources.push.ModifyStrategy;
+import org.apache.maven.index.context.IndexingContext;
+import org.apache.maven.index.updater.IndexUpdater;
+import org.apache.maven.wagon.Wagon;
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.PlexusContainerException;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.slf4j.LoggerFactory;
 
 /**
  * Хранилище пакетов Maven.
@@ -16,10 +29,25 @@ import ru.aristar.jnuget.sources.push.ModifyStrategy;
  */
 public class MavenPackageSource implements PackageSource<MavenNupkg> {
 
-    private ArtifactRepository repository;
+    private final String INDEX = "index";
+    private final String CACHE = "cache";
+    protected org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
     private String url;
+    private File storage;
+    private PlexusContainer plexusContainer;
+    private Indexer indexer;
+    private IndexUpdater indexUpdater;
+    private Wagon httpWagon;
 
     public MavenPackageSource() {
+        try {
+            plexusContainer = new DefaultPlexusContainer();
+            indexer = plexusContainer.lookup(Indexer.class);
+            indexUpdater = plexusContainer.lookup(IndexUpdater.class);
+            httpWagon = plexusContainer.lookup(Wagon.class, "http");
+        } catch (PlexusContainerException | ComponentLookupException ex) {
+            logger.error("Инициализация контейнера дала сбой.", ex);
+        }
     }
 
     public String getUrl() {
@@ -28,7 +56,36 @@ public class MavenPackageSource implements PackageSource<MavenNupkg> {
 
     public void setUrl(String url) {
         this.url = url;
-        repository = new DefaultArtifactRepository("maven", this.url, null);
+    }
+
+    public String getStorage() {
+        return storage.getAbsolutePath();
+    }
+
+    public void setStorage(String location) {
+        storage = new File(location);
+        if (!storage.exists()) {
+            storage.mkdirs();
+        }
+    }
+
+    public boolean isIndexActual() {
+        return false;
+    }
+
+    public void loadIndex() throws ComponentLookupException, IOException {
+        // Размещение служебных файлов.
+        File localCache = new File(storage, CACHE);
+        File indexDir = new File(storage, INDEX);
+        // Создаем индексаторы
+        List<IndexCreator> indexers = new ArrayList<>();
+        indexers.add(plexusContainer.lookup(IndexCreator.class, "min"));
+
+        // Контекст
+        IndexingContext context = indexer.createIndexingContext("context", "local", localCache, indexDir,
+                url, null, true, true, indexers);
+
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
